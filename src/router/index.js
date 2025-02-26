d8f4553fa0e4078dc1b55b5a9875aacb3998fca1
mengyizzz <mindy.1030529@gmail.com>
2025-02-26 17:25:06 +0800

    增加base後重新npm run build

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router';
 import DefaultLayout from '@/layouts/DefaultLayout.vue';
 import HomeView from '../views/HomeView.vue';
 import LoginView from '@/views/auth/LoginView.vue';
@@ -41,7 +41,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 })
 
