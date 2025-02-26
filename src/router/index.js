7e7de4968c5209fafeecb3a8a0ade13c414ddf05
mengyizzz <mindy.1030529@gmail.com>
2025-02-26 17:30:39 +0800

    update 分開web、hash router

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router';
 import DefaultLayout from '@/layouts/DefaultLayout.vue';
 import HomeView from '../views/HomeView.vue';
@@ -41,8 +42,14 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 })
+
+// *github page部署用
+// const router = createRouter({
+//   history: createWebHashHistory(import.meta.env.BASE_URL),
+//   routes,
+// })
 
 export default router
